e53d83882fda404baab0ec2aa7923813792de284
Cuchuflo <mily@192.168.1.108>
2026-04-27 13:32:08 -0300

    agregue la funcion de leer el archivo con voz

--- texto_desde_archivo.py
+++ texto_desde_archivo.py
@@ -1,7 +1,13 @@
 from gtts import gTTS
 import os
 
-with open('demo.txt', 'r', encoding="utf-8") as file:
+with open('demo.txt', 'r', encoding="utf-8") as file: 
         texto = file.read()
 
-print (texto)
+#print (texto)
+
+output = gTTS(texto, lang='es', slow=False)
+output.save("output_2.mp3")
+
+os.system('open output_2.mp3')
+
